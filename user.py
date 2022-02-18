7cf0690f89421192024ff0565d9335459da1987c
isaacwach <isaac.ndirangu@student.moringaschool.com>
2022-02-18 20:06:40 +0300

    created the init method and defined its properties

--- user.py
+++ user.py
@@ -5,3 +5,16 @@ class User:
     create a User class that generates new instances of a user 
     '''
     user_list = []
+    
+    def __init__(self, username, password):
+        '''
+        initialization method to define user object properties
+        args: 
+        username: name of the user 
+        password: user's password 
+        '''
+        self.username = username
+        self.password = password
+        
+    
+        
