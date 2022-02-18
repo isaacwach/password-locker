b2c1b38e101b8ace5d005ff5442485d4dbe8ec71
isaacwach <isaac.ndirangu@student.moringaschool.com>
2022-02-18 19:27:08 +0300

    created an empty credential list

---
+++ credentials.py
@@ -0,0 +1,15 @@
+import string 
+from random import choice
+'''
+import string and random modules into the file
+'''
+class Credential:
+    '''
+    class blueprint to creat instances of credentials
+    '''
+    
+    credential_list = []
+        
+        
+        
+    
