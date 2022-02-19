9890485a89ad811c7b49386f6629461b44ff8b1a
isaacwach <isaac.ndirangu@student.moringaschool.com>
2022-02-19 12:20:27 +0300

    added a method to test if we can add multiple users

--- user_test.py
+++ user_test.py
@@ -13,7 +13,7 @@ class test_user(unittest.TestCase):
     '''
     
     def setUp(self):
-        self.new_user=User(Isaac, havertz)
+        self.new_user=User("Isaac", "havertz")
         
     def tearDown(self):
         User.user_list=[]
@@ -22,9 +22,27 @@ class test_user(unittest.TestCase):
         '''
         to test whether the user object is properly initialized
         '''
-        self.assertEqual(self.new_user.username, Isaac)
-        self.assertEqual(self.new_user.password, havertz)
+        self.assertEqual(self.new_user.username, "Isaac")
+        self.assertEqual(self.new_user.password, "havertz")
         
-    if __name__ == '__main__':
-        unittest.main()
+    def test_save_user(self):
+        '''
+        To test if a user is being added to the user user_list
+        '''
+        self.new_user.save_user()
+        self.assertEqual(len(User.user_list), 1)
+    
+    def test_save_multiple_users(self):
+        '''
+        method to test if you can save more than one user object
+        '''
+        self.new_user.save_user()
+        test_user=User("Shee", "kapipi")
+        test_user1=User("Sheina", "H56ytdd")
+        
+        #save the new test users
+        test_user.save_user()
+        test_user1.save_user()
+        self.assertEqual(len(User.user_list), 3)
+        
 
