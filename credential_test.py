a6a1ee75a4639c6b50754568ea1f8c9b21e93a2e
isaacwach <isaac.ndirangu@student.moringaschool.com>
2022-02-19 20:20:59 +0300

    created a method to test if the credentialobject is properly initialized

--- credential_test.py
+++ credential_test.py
@@ -7,5 +7,17 @@ class TestCredentials(unittest.TestCase):
         
         #create a credential object test 
         self.new_credential = Credential("Isaac", "havertz", "twitter", "Uy67tye")
+       
+    def tearDown(self):
+        Credential.credential_list=[]
+        
+    def test_init(self):
+        '''
+        Testcase to test of the credential object is properly initialized 
+        '''
+        self.assertEqual(self.new_credential.user_name, "Isaac")
+        self.assertEqual(self.new_credential.user_password, "havertz")
+        self.assertEqual(self.new_credential.credential_name, "twitter")
+        self.assertEqual(self.new_credential.credential_password, "Uy67tye")
         
         
