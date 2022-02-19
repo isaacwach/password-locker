63e3b9cee0f3b03add1176d305d57235bc00ea05
isaacwach <isaac.ndirangu@student.moringaschool.com>
2022-02-19 20:08:10 +0300

    created a setup test method

--- credential_test.py
+++ credential_test.py
@@ -1,3 +1,11 @@
 import unittest
 from credentials import Credential
 from user import User
+
+class TestCredentials(unittest.TestCase):
+    def setUp(self):
+        
+        #create a credential object test 
+        self.new_credential = Credential("Isaac", "havertz", "twitter", "Uy67tye")
+        
+        
